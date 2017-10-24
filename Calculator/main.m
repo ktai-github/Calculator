04cdd8a926cff4c1a2ab15099ffe2af3da6392e5
kevint <kevint@node-1w7jr9qt5cb6gfebbeo7e0l61.ipv6.telus.net>
2017-10-23 19:53:07 -0700

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by kevint on 2017-10-23.
+//  Copyright © 2017 kevint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
