6148fffaf4eae719fc8b0a067633d9e8f5aabcb5
KevinT <kevin.tai.mail@gmail.com>
2017-10-24 16:51:00 -0700

    first commit

--- Calculator/main.m
+++ Calculator/main.m
@@ -5,13 +5,38 @@
 //  Created by kevint on 2017-10-23.
 //  Copyright © 2017 kevint. All rights reserved.
 //
+enum operation {add, subtract, multiply, divide};
+
 
 #import <Foundation/Foundation.h>
 
+static void RunOperation(enum operation currentOperation, NSInteger firstNumber, NSInteger *result, NSInteger secondNumber) {
+    switch (currentOperation) {
+        case add:
+            *result = firstNumber + secondNumber;
+            break;
+        case subtract:
+            *result = secondNumber - firstNumber;
+            break;
+        case multiply:
+            *result = firstNumber * secondNumber;
+            break;
+        case divide:
+            *result = secondNumber / firstNumber;
+            break;
+        default:
+            break;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSInteger firstNumber = 2, secondNumber = 10, result;
+        enum operation currentOperation = multiply;
+        RunOperation(currentOperation, firstNumber, &result, secondNumber);
+        
+        NSLog(@"result: %li", result);
+        
     }
     return 0;
 }
